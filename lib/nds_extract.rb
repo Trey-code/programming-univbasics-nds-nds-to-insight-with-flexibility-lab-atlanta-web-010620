6916852418d1cde64c2596709095f3e2fc438394
Treven Womack <keytonwomack@yahoo.com>
2020-01-06 13:39:31 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -31,51 +31,49 @@ def movie_with_director_name(director_name, movie_data)
 end
 
 
-# Your code after this point
+
 
 def movies_with_director_key(name, movies_collection)
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+ director_name = "Byron Poodle"
+      movies_coll = {
+        :worldwide_gross => 2,
+        :release_year => 2014,
+        :studio => "Karbit Poodles",
+        :title => "The Fire Hydrant of Doom"
+      }
+      new_movie = movie_with_director_name(director_name, movies_coll)
+
+      expect(new_movie[:director_name]).to eq(director_name)
+    end
+  end
 end
 
-
 def gross_per_studio(collection)
-  # GOAL: Given an Array of Hashes where each Hash represents a movie,
-  # return a Hash that includes the total worldwide_gross of all the movies from
-  # each studio.
-  #
-  # INPUT:
-  # * collection: Array of Hashes where each Hash where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Hash whose keys are the studio names and whose values are the sum
-  # total of all the worldwide_gross numbers for every movie in the input Hash
+   test_data = [
+      { :title => "Movie A", :studio => "Alpha Films", :worldwide_gross => 10 },
+      { :title => "Movie B", :studio => "Alpha Films", :worldwide_gross => 30 },
+      { :title => "Movie C", :studio => "Omega Films", :worldwide_gross => 30 }
+    ]
+    result = gross_per_studio(test_data)
+    expect(result["Alpha Films"]).to eq(40), "We should collect the totals of films made by this Alpha Films"
+    expect(result["Omega Films"]).to eq(30), "We should collect the totals of films made by this Omega Films"
+  end
 end
 
 def movies_with_directors_set(source)
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
-  #
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
-  #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
+ (:test_data) {
+          [
+            { :name => "Byron Poodle", :movies => [
+              { :title => "At the park" },
+              { :title => "On the couch" },
+            ]
+            },
+            { :name => "Nancy Drew", :movies => [
+              { :title => "Biting" },
+            ]
+            }
+          ] 
+        }
 end
 
 # ----------------    End of Your Code Region --------------------
