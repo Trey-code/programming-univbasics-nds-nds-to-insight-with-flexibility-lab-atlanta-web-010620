fee0acf698d8f9af30c4855a905391e2b27bd33e
Treven Womack <keytonwomack@yahoo.com>
2020-01-06 13:38:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,17 +49,15 @@ def movies_with_director_key(name, movies_collection)
 end
 
 def gross_per_studio(collection)
-  # GOAL: Given an Array of Hashes where each Hash represents a movie,
-  # return a Hash that includes the total worldwide_gross of all the movies from
-  # each studio.
-  #
-  # INPUT:
-  # * collection: Array of Hashes where each Hash where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Hash whose keys are the studio names and whose values are the sum
-  # total of all the worldwide_gross numbers for every movie in the input Hash
+   test_data = [
+      { :title => "Movie A", :studio => "Alpha Films", :worldwide_gross => 10 },
+      { :title => "Movie B", :studio => "Alpha Films", :worldwide_gross => 30 },
+      { :title => "Movie C", :studio => "Omega Films", :worldwide_gross => 30 }
+    ]
+    result = gross_per_studio(test_data)
+    expect(result["Alpha Films"]).to eq(40), "We should collect the totals of films made by this Alpha Films"
+    expect(result["Omega Films"]).to eq(30), "We should collect the totals of films made by this Omega Films"
+  end
 end
 
 def movies_with_directors_set(source)
